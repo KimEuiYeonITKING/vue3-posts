2e22e7c134d467d4b297a57479784c4ef03c9df2
euiyeon Kim <kimeuiyeon0216@gmail.com>
2023-08-31 13:34:31 +0900

    23.08.31 frist commit

--- vue3-posts/src/api/posts.js
+++ vue3-posts/src/api/posts.js
@@ -1,17 +1,21 @@
-// axios
-// ctrl + alt + 화살표 하면 여러개 체크 가능
-const posts = [
-	{ id: 1, title: '제목1', content: '내용1', createdAt: '2021-01-01' },
-	{ id: 2, title: '제목2', content: '내용2', createdAt: '2022-02-02' },
-	{ id: 3, title: '제목3', content: '내용3', createdAt: '2023-03-03' },
-	{ id: 4, title: '제목4', content: '내용4', createdAt: '2024-04-04' },
-	{ id: 5, title: '제목5', content: '내용5', createdAt: '2025-05-05' },
-];
+import { posts } from '.'; // 점만쓰면 해당 디렉토리의 index.js 파일을 가져온다.
 
-export function getPosts() {
-	return posts;
+export function getPosts(params) {
+	// return posts;
+	return posts.get('/', { params });
 }
 
 export function getPostById(id) {
-	return posts.find(item => item.id === id);
+	// return posts.find(item => item.id === id);
+	return posts.get(`/${id}`);
+}
+
+export function createPost(data) {
+	return posts.post('/', data);
+}
+export function updatePost(id, data) {
+	return posts.put(`/${id}`, data);
+}
+export function deletePost(id) {
+	return posts.delete(`/${id}`);
 }
