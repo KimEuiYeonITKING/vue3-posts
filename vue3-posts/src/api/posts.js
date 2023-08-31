0f8541608035432c6c8895f68d35523f3e22e9e7
euiyeon Kim <kimeuiyeon0216@gmail.com>
2023-08-31 15:34:07 +0900

    23.08.31 second commit

--- vue3-posts/src/api/posts.js
+++ vue3-posts/src/api/posts.js
@@ -14,8 +14,12 @@ export function createPost(data) {
 	return posts.post('/', data);
 }
 export function updatePost(id, data) {
-	return posts.put(`/${id}`, data);
+	return posts.patch(`/${id}`, data);
 }
 export function deletePost(id) {
+	// export function updatePost(id, data) {
+	// 	return posts.put(`/${id}`, data);
+	// }
+	// export function deletePost(id) {
 	return posts.delete(`/${id}`);
 }
